88676f57eec069314ac35a2deff5793114dd1a81
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2020-12-22 17:26:38 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.set({
+   width: 350,
+   height: 300,
+   dest_width: 350,
+   dest_height: 300,
+   crop_width: 350,
+   crop_height: 300,
+   image_format: 'png',
+   png_quality: 100
+});
+live_camera = document.getElementById("live-camera");
+Webcam.attach('#live-camera');
+
+function snap(){
+    Webcam.snap(function(data_url){
+        document.getElementById("photo").innerHTML = "<img src='"+ data_url + "' width='325' height='275' style='margin-top: 10px;' id='img'>";
+    });
+}
+console.log("ml5 version: " + ml5.version);
+brain_find = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/epyhIeZyZ/model.json", check);
+function check(){
+    console.log("modelLoaded");
+}
+function identify(){
+    imgGiven = document.getElementById("img");
+    brain_find.classify(imgGiven, result);
+}
+function result(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+       document.getElementById("object").innerHTML = results[0].label;
+       document.getElementById("accuracy").innerHTML = (results[0].confidence.toFixed(3)) * 100 + "%"; 
+
+       var resultName = results[0].label;
+       document.getElementById(resultName).style.backgroundColor = "grey";
+       document.getElementById(resultName).style.backgroundColor = "rgb(214, 149, 189)";
+    }
+
+
+}
+
